ab86725777295a907ae516642797a58f1e2f784d
Lunatic <lunatic@Lunatics-MacBook-Pro.local>
2019-06-01 13:10:02 +0700

    Extends Core: MY_Controller

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller 
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$model = strtolower(get_class($this));
+		if (file_exists(APPPATH . 'models/'. $model . '_model.php')) {
+			$this->load->model($model . '_model', $model, true);
+		}
+	}
+	
+	/**
+	 * Load view with default layouts
+	 *
+	 * @param [type] $data
+	 * @return void
+	 */
+	public function view($data)
+	{
+		$this->load->view('layouts/app', $data);
+	}
+
+}
+
+/* End of file MY_Controller.php */
