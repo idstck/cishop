f3b262163bd942c3504982d2336b9e2bd85bf6da
Lunatic <lunatic@Lunatics-MacBook-Pro.local>
2019-06-01 16:17:31 +0700

    Extends Core: MY_Model - Pagination

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -6,6 +6,7 @@ class MY_Model extends CI_Model
 {
 
 	protected $table	= '';
+	protected $perPage	= 5;
 
 	public function __construct()
 	{
@@ -180,6 +181,81 @@ class MY_Model extends CI_Model
 	{
 		$this->db->delete($this->table);
 		return $this->db->affected_rows();
+	}
+
+	/**
+	 * Menentukan Limit data untuk ditampilkan
+	 *
+	 * @param [type] $page
+	 * @return void
+	 */
+	public function paginate($page)
+	{
+		$this->db->limit(
+			$this->perPage,
+			$this->calculateRealOffset($page)
+		);
+	}
+
+	/**
+	 * Menggantikan offset dengan nilai sesuai halaman
+	 *
+	 * @param [type] $page
+	 * @return void
+	 */
+	public function calculateRealOffset($page)
+	{
+		if (is_null($page) || empty($page)) {
+			$offset = 0;
+		} else {
+			$offset = ($page * $this->perPage) - $this->perPage;
+		}
+
+		return $offset;
+	}
+
+	/**
+	 * Membuat Pagination dengan style bootstrap 4
+	 *
+	 * @param [type] $baseUrl
+	 * @param [type] $uriSegment
+	 * @param [type] $totalRows
+	 * @return void
+	 */
+	public function makePagination($baseUrl, $uriSegment, $totalRows = null)
+	{
+		$this->load->libary('pagination');
+
+		$config = [
+			'base_url'			=> $baseUrl,
+			'uri_segment'		=> $uriSegment,
+			'per_page'			=> $this->perPage,
+			'total_rows'		=> $totalRows,
+			'use_page_numbers'	=> true,
+			
+			'full_tag_open'		=> '<ul class="pagination">',
+			'full_tag_close'	=> '</ul>',
+			'attributes'		=> ['class' => 'page-link'],
+			'first_link'		=> false,
+			'last_link'			=> false,
+			'first_tag_open'	=> '<li class="page-item">',
+			'first_tag_close'	=> '</li>',
+			'prev_link'			=> '&laquo',
+			'prev_tag_open'		=> '<li class="page-item">',
+			'prev_tag_close'	=> '</li>',
+			'next_link'			=> '&raquo',
+			'next_tag_open'		=> '<li class="page-item">',
+			'next_tag_close'	=> '</li>',
+			'last_tag_open'		=> '<li class="page-item">',
+			'last_tag_close'	=> '</li>',
+			'cur_tag_open'		=> '<li class="page-item active"><a href="#" class="page-link">',
+			'cur_tag_close'		=> '<span class="sr-only">(current)</span></a></li>',
+			'num_tag_open'		=> '<li class="page-item">',
+			'num_tag_close'		=> '</li>',
+		];
+
+		$this->pagination->initialize($config);
+		return $this->pagination->create_link();
 	}
 }
 
