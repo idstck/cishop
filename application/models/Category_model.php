7950199782033894cd741851c789e5489bd21700
Lunatic <lunatic@Lunatics-MacBook-Pro.local>
2019-06-07 15:04:21 +0700

    Category - Create Table + Model Category

---
+++ application/models/Category_model.php
@@ -0,0 +1,37 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category_model extends MY_Model 
+{
+
+	public function getDefaultValues()
+	{
+		return [
+			'id' 	=> '',
+			'slug'	=> '',
+			'title' => ''
+		];
+	}
+
+	public function getValidationRules()
+	{
+		$validationRules = [
+			[
+				'field'	=> 'slug',
+				'label'	=> 'Slug',
+				'rules' => 'trim|required|callback_unique_slug'
+			],
+			[
+				'field'	=> 'title',
+				'label'	=> 'Kategori',
+				'rules' => 'trim|required'
+			],
+		];
+
+		return $validationRules;
+	}
+
+}
+
+/* End of file Category_model.php */
