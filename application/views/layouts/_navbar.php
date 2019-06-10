fbe5fc26f81de4959c8d5c1ee17924cba0d4e3b5
Lunatic <lunatic@Lunatics-MacBook-Pro.local>
2019-06-10 18:10:11 +0700

    Product - Make View Index Product

--- application/views/layouts/_navbar.php
+++ application/views/layouts/_navbar.php
@@ -14,7 +14,7 @@
 					<a href="#" class="nav-link dropdown-toggle" id="dropdown-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage</a>
 					<div class="dropdown-menu" aria-labelledby="dropdown-1">
 						<a href="<?= base_url('category') ?>" class="dropdown-item">Kategori</a>
-						<a href="/admin-product.html" class="dropdown-item">Produk</a>
+						<a href="<?= base_url('product') ?>" class="dropdown-item">Produk</a>
 						<a href="/admin-order.html" class="dropdown-item">Order</a>
 						<a href="/admin-users.html" class="dropdown-item">Pengguna</a>
 					</div>
---
+++ application/views/pages/product/index.php
@@ -0,0 +1,75 @@
+<main role="main" class="container">
+	<?php $this->load->view('layouts/_alert') ?>
+	<div class="row">
+		<div class="col-md-10 mx-auto">
+			<div class="card mb-3">
+				<div class="card-header">
+					<span>Produk</span>
+					<a href="/admin-product-form.html" class="btn btn-sm btn-secondary">Tambah</a>
+
+					<div class="float-right">
+						<form action="">
+							<div class="input-group">
+								<input type="text" name="keyword" class="form-control form-control-sm text-center" placeholder="Cari">
+								<div class="input-group-append">
+									<button class="btn btn-info btn-sm" type="submit">
+										<i class="fas fa-search"></i>
+									</button>
+									<a href="#" class="btn btn-info btn-sm">
+										<i class="fas fa-eraser"></i>
+									</a>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="card-body">
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Produk</th>
+								<th scope="col">Kategori</th>
+								<th scope="col">Harga</th>
+								<th scope="col">Stock</th>
+								<th scope="col"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $no = 0; foreach ($content as $row): $no++; ?>
+							<tr>
+								<td><?= $no ?></td>
+								<td>
+									<p>
+										<img src="<?= $row->image ? base_url("images/product/$row->image") : base_url("images/product/default.png") ?>" alt="" height="50">
+										<?= $row->product_title ?>
+									</p>
+								</td>
+								<td>
+									<span class="badge badge-primary"><i class="fas fa-tags"></i> <?= $row->category_title ?></span>
+								</td>
+								<td>Rp<?= number_format($row->price, 0, ',', '.') ?>,-</td>
+								<td><?= $row->is_available ? 'Tersedia' : 'Kosong' ?></td>
+								<td>
+									<a href="#">
+										<button class="btn btn-sm">
+											<i class="fas fa-edit text-info"></i>
+										</button>
+									</a>
+									<button class="btn btn-sm" type="submit">
+										<i class="fas fa-trash text-danger"></i>
+									</button>
+								</td>
+							</tr>
+							<?php endforeach ?>
+						</tbody>
+					</table>
+					
+					<nav aria-label="Page navigation example">
+						<?= $pagination ?>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
