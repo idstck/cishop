97343f874877e2863c012d95818f9d3d77bdcf45
Lunatic <lunatic@Lunatics-MacBook-Pro.local>
2019-06-17 14:24:55 +0700

    Checkout - Make create function

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -19,7 +19,7 @@ class Checkout extends MY_Controller
 		}
 	}
 
-	public function index()
+	public function index($input = null)
 	{
 		$this->checkout->table = 'cart';
 
@@ -36,11 +36,64 @@ class Checkout extends MY_Controller
 			redirect(base_url('/'));
 		}
 
-		$data['input']	= (object) $this->checkout->getDefaultValues();
+		$data['input']	= $input ? $input : (object) $this->checkout->getDefaultValues();
 		$data['title']	= 'Checkout';
 		$data['page']	= 'pages/checkout/index';
 
 		$this->view($data);
+	}
+
+	public function create()
+	{
+		if (!$_POST) {
+			redirect(base_url('checkout'));
+		} else {
+			$input = (object) $this->input->post(null, true);
+		}
+
+		if (!$this->checkout->validate()) {
+			return $this->index($input);
+		}
+
+		$total	= $this->db->select_sum('subtotal')
+					->where('id_user', $this->id)
+					->get('cart')
+					->row()
+					->subtotal;
+
+		$data = [
+			'id_user'	=> $this->id,
+			'date'		=> date('Y-m-d'),
+			'invoice'	=> $this->id.date('YmdHis'),
+			'total'		=> $total,
+			'name'		=> $input->name,
+			'address'	=> $input->address,
+			'phone'		=> $input->phone,
+			'status'	=> 'waiting'
+		];
+
+		if ($order = $this->checkout->create($data)) {
+			$cart = $this->db->where('id_user', $this->id)
+					->get('cart')->result_array();
+			foreach($cart as $row) {
+				$row['id_orders']	= $order;
+				unset($row['id'], $row['id_user']);
+				$this->db->insert('orders_detail', $row);
+			}
+
+			$this->db->delete('cart', ['id_user' => $this->id]);
+
+			$this->session->set_flashdata('success', 'Data berhasil disimpan.');
+
+			$data['title']		= 'Checkout Success';
+			$data['content']	= (object) $data;
+			$data['page']		= 'pages/checkout/success';
+
+			$this->view($data);
+		} else {
+			$this->session->set_flashdata('error', 'Oops! Terjadi suatu kesalahan.');
+			return $this->index($input);
+		}
 	}
 
 }
