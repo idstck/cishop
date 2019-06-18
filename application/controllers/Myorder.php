81d59b9b240bbd4303fbe0e67d717593b97d2e73
Lunatic <lunatic@Lunatics-MacBook-Pro.local>
2019-06-18 13:58:08 +0700

    My Order - Make Confirm Function

--- application/controllers/Myorder.php
+++ application/controllers/Myorder.php
@@ -50,6 +50,56 @@ class Myorder extends MY_Controller
 		$this->view($data);
 	}
 
+	public function confirm($invoice)
+	{
+		$data['order']	= $this->myorder->where('invoice', $invoice)->first();
+		if (!$data['order']) {
+			$this->session->set_flashdata('warning', 'Data tidak ditemukan.');
+			redirect(base_url('/myorder'));
+		}
+
+		if ($data['order']->status !== 'waiting') {
+			$this->session->set_flashdata('warning', 'Bukti transfer sudah dikiirm.');
+			redirect(base_url("myorder/detail/$invoice"));
+		}
+
+		if (!$_POST) {
+			$data['input']	= (object) $this->myorder->getDefaultValues();
+		} else {
+			$data['input']	= (object) $this->input->post(null, true);	
+		}
+
+		if (!empty($_FILES) && $_FILES['image']['name'] !== '') {
+			$imageName	= url_title($invoice, '-', true) . '-' . date('YmdHis');
+			$upload		= $this->myorder->uploadImage('image', $imageName);
+			if ($upload) {
+				$data['image']	= $upload['file_name'];
+			} else {
+				redirect(base_url("myorder/confirm/$invoice"));
+			}
+		}
+
+		if (!$this->myorder->validate()) {
+			$data['title']			= 'Konfirmasi Order';
+			$data['form_action']	= base_url("myorder/confirm/$invoice");
+			$data['page']			= 'pages/myorder/confirm';
+
+			$this->view($data);
+			return;
+		}
+
+		$this->myorder->table = 'orders_detail';
+
+		if ($this->myorder->create($data['input'])) {
+			$this->myorder->table = 'orders';
+			$this->myorder->where('id', $data['input']->id_orders)->update(['status' => 'paid']);
+			$this->session->set_flashdata('success', 'Data berhasil disimpan!');
+		} else {
+			$this->session->set_flashdata('error', 'Oops! Terjadi suatu kesalahan');
+		}
+
+		redirect(base_url("myorder/detail/$invoice"));
+	}
 }
 
 /* End of file Myorder.php */
